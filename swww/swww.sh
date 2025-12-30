93f3e12402a2f4d31fb0f7191dea4ef3f47e3431
Tareq Alansari <tareq.ayz@gmail.com>
2025-12-30 12:39:00 -0800

    quick comment linking to original post

--- swww/swww.sh
+++ swww/swww.sh
@@ -1,4 +1,5 @@
 !/usr/bin/bash
+# swww setup from https://www.reddit.com/r/hyprland/comments/1df3c93/tutorial_swww_how_to_setup/
 #start swww
 WALLPAPERS_DIR=~/Pictures/wallpapers/current/
 WALLPAPER=$(find "$WALLPAPERS_DIR" -type f | shuf -n 1)
