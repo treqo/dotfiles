2a0d86e322cd4b2af95234014ef331620cf49207
Tareq Alansari <tareq.ayz@gmail.com>
2026-04-05 20:32:03 +0400

    adding claude and ms office

---
+++ bar/sketchybar/items/claude.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#### Claude
+
+sketchybar --add item claude right \
+  --set claude \
+  icon.font="sketchybar-app-font:Regular:20.0" \
+  icon=":claude:"
---
+++ bar/sketchybar/items/ms_office.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#### MS_Office – suite of MS tools
+
+sketchybar --add item ms_office right \
+  --set ms_office \
+  icon.font="Hack Nerd Font:Regular:24.0" \
+  icon="󰏆"
